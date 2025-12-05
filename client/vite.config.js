a40ee1ca40549f6380f7fe276b727e52a6e2f03a
Yina Mu <yinamu9@gmail.com>
2025-12-04 16:11:09 -0800

    sync client configs and tests

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 添加一些关于test的配置
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.js",
+  },
+});
