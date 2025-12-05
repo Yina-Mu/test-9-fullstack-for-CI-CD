fbc2d800ac60e0ec332c5a1e04d43a806609f24c
Yina Mu <yinamu9@gmail.com>
2025-12-04 16:31:06 -0800

    delete import vitest for client

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import vitest from "eslint-plugin-vitest";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
